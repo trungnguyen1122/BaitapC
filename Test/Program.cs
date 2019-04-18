9cabe65ca0f243c6b885c58486816bd6f30a821a
trung <trungntd00752@fpt.edu.vn>
2019-04-18 19:38:04 +0700

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        public int age =21;
+        public static int year = 2019;
+        private const int century = 21;
+        public static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Hello World!");
+                Program mc = new Program();
+                Console.WriteLine("age: "+ mc.age);
+                Console.WriteLine("year: "+ Program.year);
+                Console.WriteLine("century: "+ Program.century);
+                
+            
+        }
+    }
+}
