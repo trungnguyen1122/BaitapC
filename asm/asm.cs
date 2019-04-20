7dac538353bd6e4475a98b4e265629653789ef31
trung <trungntd00752@fpt.edu.vn>
2019-04-20 20:28:14 +0700

    1

--- asm/asm.cs
+++ asm/asm.cs
@@ -6,24 +6,22 @@ namespace asm
     {
           public static void Main(string[] args)
                 {
-                     int prv = 0, pre = 1, trm, i, n;
+                     int n1 = 0, n2 = 1, trm, i, n;
         
                     Console.Write("\n");
-                    Console.Write("In day Fibonacci trong C#:\n");
-                    Console.Write("-------------------------");
-                    Console.Write("\n\n");
+                    Console.Write("In day Fibonacci trong C#:\n\n");
         
                     Console.Write("Nhap so phan tu can hien thi: ");
                     n = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Day Fibonacci gom {0} phan tu la: \n", n);
-                    Console.Write("{0}    {1}", prv, pre);
+                    Console.Write("{0}    {1}", n1, n2);
         
                     for (i = 3; i <= n; i++)
                     {
-                        trm = prv + pre;
+                        trm = n1+ n2;
                         Console.Write("  {0}  ", trm);
-                        prv = pre;
-                        pre = trm;
+                        n1 = n2;
+                        n2 = trm;
                     }
                     Console.Write("\n");   
         
